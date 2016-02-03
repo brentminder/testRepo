3a09428ae9a3a824f7dc6aca1aefa0bb7f659d53
brentminder <bminder@gmail.com>
2016-02-02 21:27:36 -0500

    1st commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("This is a test console app.");
+			Console.ReadKey();
+		}
+	}
+}
